d27a692c0e439be6de142d47178c0d07067ce258
codwerk <louis_widdy@live.fr>
2020-10-10 02:18:36 +0200

    install boostrap 5

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap/js/dist/collapse'
+
+Vue.use(bootstrap);
 
 Vue.config.productionTip = false
 
 new Vue({
+  bootstrap,
   render: h => h(App),
 }).$mount('#app')
