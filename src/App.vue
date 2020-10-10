35425ee6665bbeb695335bdd03e463c0dc0f9ccc
codwerk <louis_widdy@live.fr>
2020-10-10 14:30:57 +0200

    intégration html/css, objets: questions

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,73 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
 
+    <div class="d-flex justify-content-center">
+      <h1 class="bd-title" id="content">Create Accordion For FAQ</h1>
+    </div>
 
+  <div class="container">
     <div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header p-0" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-light btn-block text-left p-3 rounded-0" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h2>
+    <div class="card" v-for="(question, index) in questions" :key="index">
+      <div class="card-header p-0" :id="'heading'+index" @click="question.open = !question.open">
+        <h2 class="mb-0">
+          <button class="btn btn-light btn-block text-left p-3 rounded-0" type="button" data-toggle="collapse" :data-target="'#'+question.order">
+            {{ question.title }}
+          </button>
+        </h2>
+      </div>
+        <div :id="question.order" v-show="question.open" class="collapse">
+          <div class="card-body text-left">
+            {{ question.reply }}
+          </div>
+        </div>
     </div>
+  </div>
+  </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header p-0" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-light btn-block text-left collapsed p-3 rounded-0" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header p-0" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-light btn-block text-left collapsed p-3 rounded-0" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      questions: {
+        "Question1": {
+          "open": false,
+          "title": "One: What Happen When Bla Bla Bla ?",
+          "reply": "You can Do What You Want",
+          "order": "first"
+        },
+        "Question2": {
+          "open": false,
+          "title": "Two: What Happen When Bla Bla Bla ?",
+          "reply": "You can Do What You Want",
+          "order": "second"
+        },
+        "Question3": {
+          "open": false,
+          "title": "Three: What Happen When Bla Bla Bla ?",
+          "reply": "You can Do What You Want",
+          "order": "third"
+        },
+        "Question4": {
+          "open": false,
+          "title": "Four: What Happen When Bla Bla Bla ?",
+          "reply": "You can Do What You Want",
+          "order": "fourth"
+        },
+        "Question5": {
+          "open": false,
+          "title": "Five: What Happen When Bla Bla Bla ?",
+          "reply": "You can Do What You Want",
+          "order": "fifth"
+        }
+      }
+    }
   }
 }
 </script>
